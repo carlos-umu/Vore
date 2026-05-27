0e54dc10f6880f1e301ba0a0c8c31ee93486dcc7
carlos-umu <carlos.p.p@um.es>
2026-05-27 19:23:14 +0200

    WinScreen y GameOverScreen hechas

--- Vore/Screens/GameOverScreen.cs
+++ Vore/Screens/GameOverScreen.cs
@@ -1,27 +1,42 @@
 using Raylib_cs;
 
-public class GameOverScreen 
+public class GameOverScreen
 {
 
-     public static GameState Update(int width, int height)
+    public static GameState Update(int width, int height)
     {
         if (Raylib.IsKeyPressed(KeyboardKey.Enter))
         {
-            return GameState.Menu;
-        }        
+            return GameState.Playing;
+        }
         return GameState.GameOver;
-       
+
     }
 
-    public static void Draw( int width, int height)
+    public static void Draw(int width, int height)
     {
-        
-        int titleY = height / 2 - 15;     
-        int titleX = width / 2- Raylib.MeasureText("GAME OVER", 30) / 2;
-        Raylib.DrawText("GAME OVER", titleX, titleY, 30, Color.Red);
-        Raylib.DrawText("Press Enter to Return to Menu", titleX - 50, titleY + 80, 20, Color.White);
-        Raylib.DrawText("By: @9C", 1800, 990, 20, Color.Red);
+
+        Raylib.DrawRectangle(0, 0, width, height, new Color(0, 0, 0, 200));
+
+        string title = "L E V E L   F A I L E D";
+        int titleSize = 100;
+        int titleX = width / 2 - Raylib.MeasureText(title, titleSize) / 2;
+        int titleY = height / 2 - 100;
+
+        Raylib.DrawText(title, titleX + 6, titleY + 6, titleSize, Color.Black);
+        Raylib.DrawText(title, titleX, titleY, titleSize, Color.Red);
+
+        double time = Raylib.GetTime();
+        int alpha = (int)((Math.Sin(time * 6) + 1.0) * 127.5);
+        Color pulseColor = new Color(255, 255, 255, alpha);
+
+        string enterText = "- Press ENTER to continue -";
+        int enterSize = 30;
+        int enterX = width / 2 - Raylib.MeasureText(enterText, enterSize) / 2;
+        Raylib.DrawText(enterText, enterX, titleY + 150, enterSize, pulseColor);
+
+
+        Raylib.DrawText("By: @9C", 30, height - 50, 20, Color.Gray);
     }
 }
-    
-   
+
--- Vore/Screens/WinScreen.cs
+++ Vore/Screens/WinScreen.cs
@@ -16,11 +16,27 @@ public class WinScreen
     public static void Draw(int width, int height)
     {
 
-        int titleY = height / 2 - 15;
-        int titleX = width / 2 - Raylib.MeasureText("WIN", 30) / 2;
-        Raylib.DrawText("WIN", titleX, titleY, 30, Color.Red);
-        Raylib.DrawText("Press Enter to Return to Menu", titleX - 50, titleY + 80, 20, Color.White);
-        Raylib.DrawText("By: @9C", 1800, 990, 20, Color.Red);
+        Raylib.DrawRectangle(0, 0, width, height, new Color(0, 0, 0, 200));
+
+        string title = "L E V E L   C O M P L E T E D";
+        int titleSize = 100;
+        int titleX = width / 2 - Raylib.MeasureText(title, titleSize) / 2;
+        int titleY = height / 2 - 100;
+
+        Raylib.DrawText(title, titleX + 6, titleY + 6, titleSize, Color.Black);
+        Raylib.DrawText(title, titleX, titleY, titleSize, Color.Green);
+
+        double time = Raylib.GetTime();
+        int alpha = (int)((Math.Sin(time * 6) + 1.0) * 127.5);
+        Color pulseColor = new Color(255, 255, 255, alpha);
+
+        string enterText = "- Press ENTER to continue -";
+        int enterSize = 30;
+        int enterX = width / 2 - Raylib.MeasureText(enterText, enterSize) / 2;
+        Raylib.DrawText(enterText, enterX, titleY + 150, enterSize, pulseColor);
+
+
+        Raylib.DrawText("By: @9C", 30, height - 50, 20, Color.Gray);
     }
 
 
